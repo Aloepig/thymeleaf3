0cb667803efd18c83e2e721d88ca4fabbacc2bd3
leebyungjik <aloepig@gmail.com>
2023-01-12 11:06:12 +0900

    테스트와 클래스

---
+++ src/main/java/com/myalley/thymeleaf3/domain/tdd/Money.java
@@ -0,0 +1,48 @@
+package com.myalley.thymeleaf3.domain.tdd;
+
+import java.util.Objects;
+
+public class Money {
+
+    private final int amount;
+    private final String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    public Money times(int multi) {
+        return new Money(amount * multi, currency);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Money money)) {
+            return false;
+        }
+        return amount == money.amount && Objects.equals(currency, money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
--- src/test/java/com/myalley/thymeleaf3/TDD/MoneyTest.java
+++ src/test/java/com/myalley/thymeleaf3/TDD/MoneyTest.java
@@ -1,9 +1,8 @@
 package com.myalley.thymeleaf3.TDD;
 
+import com.myalley.thymeleaf3.domain.tdd.Money;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.Objects;
 
 public class MoneyTest {
 
@@ -19,52 +18,6 @@ public class MoneyTest {
         Assertions.assertEquals(Money.dollar(5), Money.dollar(5));
         Assertions.assertNotEquals(Money.dollar(6), Money.dollar(5));
         Assertions.assertEquals(Money.franc(5), Money.franc(5));
-    }
-
-
-
-    static class Money {
-        private final int amount;
-        private final String currency;
-
-        public Money(int amount, String currency) {
-            this.amount = amount;
-            this.currency = currency;
-        }
-
-        public static Money dollar(int amount) {
-            return new Money(amount, "USD");
-        }
-
-        public static Money franc(int amount) {
-            return new Money(amount, "CHF");
-        }
-
-        Money times(int multi) {
-            return new Money(amount * multi, currency);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Money money)) {
-                return false;
-            }
-            return amount == money.amount && Objects.equals(currency, money.currency);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(amount);
-        }
-
-        @Override
-        public String toString() {
-            return "Money{" +
-                    "amount=" + amount +
-                    ", currency='" + currency + '\'' +
-                    '}';
-        }
     }
 
 }
